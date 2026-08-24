70f8a989c9d01960065b99ecb645006ef1d7470b
davi-chagas <davi.chagas@sptech.school>
2026-08-24 20:23:50 -0300

    feat: alterações no script do projeto pi

--- database/projeto-pi.sql
+++ database/projeto-pi.sql
@@ -10,33 +10,59 @@ CREATE TABLE usuario(
     email VARCHAR(60) UNIQUE NOT NULL,
     senha VARCHAR(12) NOT NULL,
     dt_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
-    status TINYINT
+    status TINYINT DEFAULT 1, -- por default ativo
+    role VARCHAR(10),
+    CONSTRAINT chRole CHECK (role IN('ADMIN', 'USUARIO'))
 );
 
 CREATE TABLE empresa (
 	id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(45) NOT NULL,
     cnpj CHAR(14) NOT NULL UNIQUE,
-    dt_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
-    status TINYINT
+    logradouro VARCHAR(40) NOT NULL,
+    cidade VARCHAR(45) NOT NULL,
+    estado CHAR(2) NOT NULL,
+    cep CHAR(8),
+	dt_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status TINYINT DEFAULT 1 -- por default ativo
+);
+
+CREATE TABLE unidade (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    empresa_id INT NOT NULL, -- referencia logica para qual empresa pertencer
+    nome VARCHAR(60) NOT NULL,
+    logradouro VARCHAR(100) NOT NULL,
+    cidade VARCHAR(45) NOT NULL,
+    estados CHAR(2) NOT NULL,
+    cep CHAR(8)
 );
 
 CREATE TABLE sensor (
-	id  INT PRIMARY KEY AUTO_INCREMENT,
-    codigo VARCHAR(45),
-    distancia DECIMAL (3, 1),
-    nivel VARCHAR(40),
-    CONSTRAINT chNivel CHECK(nivel IN('baixo', 'medio', 'alto', 'critico')),
-    alerta TINYINT, -- se possui alerta 
-	status TINYINT
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    tolva_id INT,
+	status TINYINT DEFAULT 1 -- por default ativo
+);
+
+CREATE TABLE leitura_sensor (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    sensor_id INT NOT NULL,
+    distancia DECIMAL (5,2) NOT NULL, -- em centimetros
+    nivel_percentual DECIMAL(5,2) NOT NULL,
+    estado VARCHAR (10) NOT NULL
+    CONSTRAINT chEstado CHECK (estado IN('baixo', 'médio', 'alto', 'critico')),
+    -- alerta
+    dt_leitura DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE tolva (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-    capacidade DECIMAL (3,1) NOT NULL,
-    residuo VARCHAR(45) NOT NULL,
-    CONSTRAINT chResiduo CHECK (residuo IN('ossos', 'sangue'))
+    unidade_id INT NOT NULL,
+    altura DECIMAL (5,2) NOT NULL,
+    capacidade DECIMAL (10,2) NOT NULL,
+    residuo VARCHAR(45) NOT NULL
 );
+
+-- COMANDOS DE ADMIN
 
 -- LISTAR TODOS OS USUARIOS
 SELECT * FROM usuario;
@@ -49,15 +75,32 @@ WHERE status != 0;
 SELECT * FROM usuario
 WHERE status = 0;
 
--- Monitoramento de Tolvas
+-- LISTAR TODAS AS EMPRESAS
+SELECT * from empresa;
+
+-- Listar todas as empresas ativas
+SELECT * FROM empresa
+WHERE status != 0;
+
+-- Listar todas as empresas inativas;
+SELECT * FROM empresa
+WHERE status = 0;
+
+
+-- MONITORAMENTO DAS TOLVAS
 
 -- Contar a quantidade de tolvas não nulls.
 SELECT COUNT(id) FROM tolva;
 
-SELECT COUNT(alerta) FROM sensor;
+-- Capacidade tolva
+SELECT id, capacidade FROM tolva;
 
+-- Exibir o id e codigo do sensor do qual alerta é TRUE
 SELECT id, codigo from sensor
 WHERE alerta = 1;
 
-DROP TABLE sensor;
+
+
+
+
 
