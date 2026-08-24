d179cb7e7e4e6781c8c4c5cce338a57275571acb
davi-chagas <davi.chagas@sptech.school>
2026-08-24 13:29:53 -0300

    feat: alterando script aula 04

--- database/aula04.sql
+++ database/aula04.sql
@@ -10,3 +10,20 @@ CREATE TABLE treinador (
 );
 
 ALTER TABLE treinador AUTO_INCREMENT = 100;
+
+INSERT INTO treinador VALUES
+(DEFAULT, 'Ash Ketchum', 12, '11956482366', NOW()),
+(DEFAULT, 'Misty', 12, NULL, NOW()),
+(DEFAULT, 'Brocky', 15, '11926482366', NOW()),
+(DEFAULT, 'Lance', 60, NULL, NOW()),
+(DEFAULT, 'Serena', 14, '11923487512', NOW());
+
+SELECT * FROM treinador;
+
+SELECT nome, DATE_FORMAT(dataCadastro, '%D/%m/%Y %H:%m:%s') AS data_cadastro
+FROM treinador;
+
+
+-- preciso especificar todos os campos 
+INSERT INTO treinador (nome, idade, telefone, dataCadastro) VALUES 
+();
