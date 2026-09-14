df81b6c89e9b37024893ad2b477e295f2bdd72ae
davi-chagas <davi.chagas@sptech.school>
2026-09-14 16:09:20 -0300

    feat: mais consultas

--- database/sprint2/aula1.sql
+++ database/sprint2/aula1.sql
@@ -34,4 +34,40 @@ INSERT INTO sprint2.produto VALUES
 
 SELECT * FROM produto;
 
+SELECT * FROM sprint2.produto 
+JOIN sprint2.loja;
 
+INSERT INTO sprint2.produto VALUES 
+(DEFAULT, 'Monster', 14.90, 'Energético' ,'22385678', '2026-02-05', NOW(), 1),
+(DEFAULT, 'Negresco', 3.55, 'bolacha' ,'52345678', '2026-11-14', NOW(), 1),
+(DEFAULT, 'Miojo - turma da monica', 3.00, 'macarrão' ,'02345678', '2030-04-05', NOW(), 1);
+
+INSERT INTO sprint2.loja (nome, cnpj, segmento) VALUES 
+('Pernambucanas', '356.785.0001/1', 'Varejo');
+
+-- join correto
+SELECT * FROM produto 
+JOIN loja
+ON loja.id = produto.fk_loja;
+
+-- todos os produtos e todas as lojas
+SELECT * FROM produto 
+RIGHT JOIN loja ON loja.id = produto.fk_loja;
+
+-- criando apelidos para as tabelas
+SELECT * FROM produto AS p
+JOIN loja as l On l.id = p.fk_loja;
+
+SELECT 
+CONCAT(p.nome, ' - ',l.nome ) AS produto_loja
+FROM produto AS p
+JOIN loja AS l ON l.id = p.fk_loja;
+
+UPDATE produto 
+SET fk_loja = null
+where id = 6;
+
+SELECT 
+CONCAT(p.nome, ' - ', IFNULL(l.nome, 'SEM LOJA') ) AS produto_loja
+FROM produto AS p
+LEFT JOIN loja AS l ON l.id = p.fk_loja;
